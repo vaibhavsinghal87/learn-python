864a512b7d29b28e18462e80bbda21241f7a5955
Vaibhav Singhal <vaibhavsinghal87@gmail.com>
2024-05-20 18:02:01 +0000

    python basics

---
+++ basics/string.py
@@ -0,0 +1,21 @@
+print(type(""));
+
+age = 85;
+print(age);
+
+age = "eighty-five"
+print(age);
+
+first_name = 'John';
+last_name = 'Doe';
+print(first_name + last_name);
+
+print("4" + "5");
+
+msg = "Hello World!"
+print(msg[0]);
+print(msg[-1]);
+
+user = None;
+print(user);
+
---
+++ basics/variables.py
@@ -0,0 +1,6 @@
+age = 25;
+print(age + 1);
+print(age * 3);
+
+age += 5;
+print(age);
